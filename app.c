26a166b62c4cc0659a3b022d3cbc0da3a52eb5b6
mohosin2126 <mohosin2126@gmail.com>
2024-09-20 01:33:31 +0600

    variable done

--- app.c
+++ app.c
@@ -1,8 +1,33 @@
 // first print
 
-#include<stdio.h>
-int main (){
+// stdio.h=> standard(std) input (i) output(o) header(h)
 
-    printf("hello there this is me ");
+// #include<stdio.h>
+// int main (){
+//     printf("hello there this is me ");
+//     return 0;
+// }
+
+
+// variables 
+
+#include<stdio.h>
+
+int main() {
+    const int score = 95;
+    const double pi = 3.14159265359;
+    const float radius = 7.5f;
+    const char grade = 'A';
+    const long population = 7800000000;
+
+    printf("Integer: %d\n", score);
+    printf("Double: %.10lf\n", pi);
+    printf("Float: %.2f\n", radius);
+    printf("Character: %c\n", grade);
+    printf("LongInteger: %ld\n", population);
+
     return 0;
 }
+
+
+
