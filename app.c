a1e72009a19be1b9faf722befdd97099f94a98ad
mohosin2126 <mohosin2126@gmail.com>
2024-09-21 23:19:35 +0600

    user input and output using gets and puts method

--- app.c
+++ app.c
@@ -11,23 +11,32 @@
 
 // variables 
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main() {
-    const int score = 95;
-    const double pi = 3.14159265359;
-    const float radius = 7.5f;
-    const char grade = 'A';
-    const long population = 7800000000;
+// int main() {
+//     const int score = 95;
+//     const double pi = 3.14159265359;
+//     const float radius = 7.5f;
+//     const char grade = 'A';
+//     const long population = 7800000000;
 
-    printf("Integer: %d\n", score);
-    printf("Double: %.10lf\n", pi);
-    printf("Float: %.2f\n", radius);
-    printf("Character: %c\n", grade);
-    printf("LongInteger: %ld\n", population);
+//     printf("Integer: %d\n", score);
+//     printf("Double: %.10lf\n", pi);
+//     printf("Float: %.2f\n", radius);
+//     printf("Character: %c\n", grade);
+//     printf("LongInteger: %ld\n", population);
 
-    return 0;
+//     return 0;
+// }
+
+
+// use input and output using gets and puts method 
+#include <stdio.h>
+int main (){
+char name[50];
+printf("enter your name here:");
+gets(name);
+printf("hello \n ");
+puts(name);
 }
-
-
 
