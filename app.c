08ced01b1abfd1dbfeac313a5bad4b3209805a4c
mohosin2126 <mohosin2126@gmail.com>
2024-09-21 23:20:37 +0600

    comment out all function

--- app.c
+++ app.c
@@ -2,32 +2,32 @@
 
 // stdio.h=> standard(std) input (i) output(o) header(h)
 
-// #include<stdio.h>
-// int main (){
-//     printf("hello there this is me ");
-//     return 0;
-// }
+#include<stdio.h>
+int main (){
+    printf("hello there this is me ");
+    return 0;
+}
 
 
 // variables 
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main() {
-//     const int score = 95;
-//     const double pi = 3.14159265359;
-//     const float radius = 7.5f;
-//     const char grade = 'A';
-//     const long population = 7800000000;
+int main() {
+    const int score = 95;
+    const double pi = 3.14159265359;
+    const float radius = 7.5f;
+    const char grade = 'A';
+    const long population = 7800000000;
 
-//     printf("Integer: %d\n", score);
-//     printf("Double: %.10lf\n", pi);
-//     printf("Float: %.2f\n", radius);
-//     printf("Character: %c\n", grade);
-//     printf("LongInteger: %ld\n", population);
+    printf("Integer: %d\n", score);
+    printf("Double: %.10lf\n", pi);
+    printf("Float: %.2f\n", radius);
+    printf("Character: %c\n", grade);
+    printf("LongInteger: %ld\n", population);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 // use input and output using gets and puts method 
