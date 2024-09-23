8608a3d9051483940aa5df0fe0520eaa57fa2a72
mohosin2126 <mohosin2126@gmail.com>
2024-09-23 16:03:59 +0600

    rearranged the file

--- app.c
+++ app.c
@@ -73,3 +73,51 @@ int main() {
     printf("%f", fahrenheit);
     return 0;
 }
+#include <stdio.h>
+
+// First Print
+int main() {
+    printf("Hello there, this is me\n");
+
+    // Variables
+    const int score = 95;
+    const double pi = 3.14159265359;
+    const float radius = 7.5f;
+    const char grade = 'A';
+    const long population = 7800000000;
+
+    printf("\n--- Variables ---\n");
+    printf("Integer: %d\n", score);
+    printf("Double: %.10lf\n", pi);
+    printf("Float: %.2f\n", radius);
+    printf("Character: %c\n", grade);
+    printf("Long Integer: %ld\n", population);
+
+    // Input and Output using gets and puts
+    char name[50];
+    printf("\n--- Input/Output ---\n");
+    printf("Enter your name here: ");
+    gets(name);
+    printf("Hello\n");
+    puts(name);
+
+    // Size of Integer
+    int i;
+    printf("\n--- Size of Integer ---\n");
+    printf("Size of the integer: %d bytes\n", sizeof(i));
+
+    // Convert ASCII value
+    int num;
+    printf("\n--- ASCII Conversion ---\n");
+    printf("Enter your number here: ");
+    scanf("%d", &num);
+    printf("The ASCII value is: %c\n", num);
+
+    // Celsius to Fahrenheit
+    float celsius = 20.5;
+    float fahrenheit = (celsius * 9 / 5) + 32;
+    printf("\n--- Celsius to Fahrenheit ---\n");
+    printf("Celsius: %.2f => Fahrenheit: %.2f\n", celsius, fahrenheit);
+
+    return 0;
+}
--- tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main (){
-    printf("hello there this is me ");
-    return 0;
-}
