34b423a981561fcb367b17a61e90a74aa5519a4e
mohosin2126 <mohosin2126@gmail.com>
2024-09-29 19:49:41 +0600

    added find largest number

--- elseif.c
+++ elseif.c
@@ -1,15 +1,34 @@
+// #include <stdio.h>
+
+// int main() {
+//     int num;
+//     printf("Please enter a number you want: ");
+
+//     scanf("%d", &num);
+
+//     if (num > 50) {
+//         printf("You can buy the product.\n");
+//     } else {
+//         printf("Sorry, you will not get the product.\n");
+//     }
+
+//     return 0;
+// }
+
+
+// Largest Number Finder
+
 #include <stdio.h>
 
 int main() {
-    int num;
-    printf("Please enter a number you want: ");
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
 
-    scanf("%d", &num);
-
-    if (num > 50) {
-        printf("You can buy the product.\n");
+    if (num1 > num2) {
+        printf("%d is the largest number.\n", num1);
     } else {
-        printf("Sorry, you will not get the product.\n");
+        printf("%d is the largest number.\n", num2);
     }
 
     return 0;
