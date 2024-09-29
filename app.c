a9e2b7defa966ae3d331d06ce6a63ad6e13a0bc9
mohosin2126 <mohosin2126@gmail.com>
2024-09-29 19:53:12 +0600

    rearranged the app.c file

--- app.c
+++ app.c
@@ -1,85 +1,12 @@
-// first print
-
-// stdio.h=> standard(std) input (i) output(o) header(h)
-
-#include<stdio.h>
-int main (){
-    printf("hello there this is me ");
-    return 0;
-}
-
-
-// variables 
-
-#include<stdio.h>
-
-int main() {
-    const int score = 95;
-    const double pi = 3.14159265359;
-    const float radius = 7.5f;
-    const char grade = 'A';
-    const long population = 7800000000;
-
-    printf("Integer: %d\n", score);
-    printf("Double: %.10lf\n", pi);
-    printf("Float: %.2f\n", radius);
-    printf("Character: %c\n", grade);
-    printf("LongInteger: %ld\n", population);
-
-    return 0;
-}
-
-
-// use input and output using gets and puts method 
-#include <stdio.h>
-int main (){
-char name[50];
-printf("enter your name here:");
-gets(name);
-printf("hello \n ");
-puts(name);
-}
-
-
-
-#include<stdio.h>
-int main()
-{
-    int i; 
-    printf("size of the integer: %d",sizeof(i));
-    return 0;
-}
-
-// convert ascii value 
-
-#include<stdio.h>
-int main (){
-int num;
-printf("Enter Your Number here:");
-scanf("%d", &num);
-printf("The ascii value is : %c",num);
-return 0;
-}
-
-
-
-// celsius to farenheit 
-
-#include <stdio.h>
-
-int main() {
-    float celsius = 20.5;
-    float fahrenheit = (celsius * 9/5) + 32;
-    printf("%f", fahrenheit);
-    return 0;
-}
 #include <stdio.h>
 
 // First Print
-int main() {
+void printHello() {
     printf("Hello there, this is me\n");
+}
 
-    // Variables
+// Variables
+void printVariables() {
     const int score = 95;
     const double pi = 3.14159265359;
     const float radius = 7.5f;
@@ -92,32 +19,49 @@ int main() {
     printf("Float: %.2f\n", radius);
     printf("Character: %c\n", grade);
     printf("Long Integer: %ld\n", population);
+}
 
-    // Input and Output using gets and puts
+// Input and Output using gets and puts
+void inputOutput() {
     char name[50];
     printf("\n--- Input/Output ---\n");
     printf("Enter your name here: ");
-    gets(name);
+    gets(name);  // Note: Using gets is not recommended due to safety concerns; consider using fgets instead.
     printf("Hello\n");
     puts(name);
+}
 
-    // Size of Integer
+// Size of Integer
+void printSizeOfInteger() {
     int i;
     printf("\n--- Size of Integer ---\n");
-    printf("Size of the integer: %d bytes\n", sizeof(i));
+    printf("Size of the integer: %d bytes\n", (int)sizeof(i));
+}
 
-    // Convert ASCII value
+// Convert ASCII value
+void asciiConversion() {
     int num;
     printf("\n--- ASCII Conversion ---\n");
     printf("Enter your number here: ");
     scanf("%d", &num);
     printf("The ASCII value is: %c\n", num);
+}
 
-    // Celsius to Fahrenheit
+// Celsius to Fahrenheit
+void celsiusToFahrenheit() {
     float celsius = 20.5;
     float fahrenheit = (celsius * 9 / 5) + 32;
     printf("\n--- Celsius to Fahrenheit ---\n");
     printf("Celsius: %.2f => Fahrenheit: %.2f\n", celsius, fahrenheit);
+}
+
+int main() {
+    printHello();
+    printVariables();
+    inputOutput();
+    printSizeOfInteger();
+    asciiConversion();
+    celsiusToFahrenheit();
 
     return 0;
 }
