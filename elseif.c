48ba395d1e1e544e51633ef4aff1773710eb9133
mohosin2126 <mohosin2126@gmail.com>
2024-09-29 19:51:34 +0600

    arranged else is code

--- elseif.c
+++ elseif.c
@@ -1,28 +1,20 @@
-// #include <stdio.h>
-
-// int main() {
-//     int num;
-//     printf("Please enter a number you want: ");
-
-//     scanf("%d", &num);
-
-//     if (num > 50) {
-//         printf("You can buy the product.\n");
-//     } else {
-//         printf("Sorry, you will not get the product.\n");
-//     }
-
-//     return 0;
-// }
-
-
-// Largest Number Finder
-
 #include <stdio.h>
 
 int main() {
-    int num1, num2;
-    printf("Enter two numbers: ");
+    int num, num1, num2;
+
+    // First part: Buying product based on number input
+    printf("Please enter a number you want: ");
+    scanf("%d", &num);
+
+    if (num > 50) {
+        printf("You can buy the product.\n");
+    } else {
+        printf("Sorry, you will not get the product.\n");
+    }
+
+    // Second part: Finding the largest number between two inputs
+    printf("\nEnter two numbers: ");
     scanf("%d %d", &num1, &num2);
 
     if (num1 > num2) {
