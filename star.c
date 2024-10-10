5c9a31a704cad4043dac6f0e523eda16e5473898
mohosin2126 <mohosin2126@gmail.com>
2024-10-10 12:02:38 +0600

    print star

---
+++ star.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+
+for (int i=0;i<6;i++){
+
+    // printf("the value of i : %d\n ",i);
+    for (int j=0;j<5;j++){
+        printf("* ");
+    }
+    printf("\n");
+}
+return 0;
+
+}
