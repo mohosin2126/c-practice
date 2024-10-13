b924cc723367d4b643ca324f274489346fd9d1bf
mohosin2126 <mohosin2126@gmail.com>
2024-10-13 10:45:49 +0600

    print nested loop and star

---
+++ loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n = 6;
+    for (int i = 1; i <= 6; i++)
+    {
+
+        for (int j = 0; j < 6; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
