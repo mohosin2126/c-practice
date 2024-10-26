ed5e3822ad0437ab49001ebed887bb7fa95d00dd
mohosin2126 <mohosin2126@gmail.com>
2024-10-26 23:16:14 +0600

    basic array input and printf solved

--- array.c
+++ array.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-
-int main() {
+int main()
+{
     int n;
     scanf("%d", &n);
-    
-    // Correct way to declare an array of size n
     int a[n];
-    
-    // Reading values into the array
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&a[i]);
     }
-    
-    // Access the last element
-    printf("%d\n", a[n - 1]); 
-    
-    // Print all elements in the array
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
+
+    for (int i = 0; i < n; i++)
+    {
+
+        printf("%d", a[i]);
     }
-    
+
     return 0;
 }
