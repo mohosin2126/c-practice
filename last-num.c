931cf3ec14f045be94829a68c4f866dbbbe6b0e2
mohosin2126 <mohosin2126@gmail.com>
2024-10-26 23:29:19 +0600

    last number access done

--- last-num.c
+++ last-num.c
@@ -9,11 +9,8 @@ int main()
         scanf("%d", &a[i]);
     }
 
-    for (int i = 0; i < n; i++)
-    {
-
-        printf("%d", a[i]);
-    }
+    int lastNumber = a[n - 1];
+    printf("%d", lastNumber);
 
     return 0;
 }
